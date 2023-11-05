e9960ec1bc889e35d0e68fef4c5a8b020e9a644b
Moazam Ali <mozalikorai@gmail.com>
2023-11-05 20:27:39 +0500

    updated the admin routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import HomePage from "./components/pages/home/home-page";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import DonateBloodPage from "./components/pages/donate-blood/donate-blood-page";
 import HostBloodDrivePage from "./components/pages/host-blood-drive/host-blood-drive";
 import NeedBloodPage from "./components/pages/need-blood/need-blood-page";
 import ContactPage from "./components/pages/contact/contact-page";
 import Admin from "./components/layouts/admin";
+
+import Dashboard from "../src/components/views/admin/dashboard";
+import AdminDonateBlood from "../src/components/views/admin/admin-donate-blood";
+import AdminNeedBlood from "../src/components/views/admin/admin-need-blood";
+import AdminHostBloodDrive from "../src/components/views/admin/admin-host-blood-drive";
+import AdminNeedHelp from "../src/components/views/admin/admin-need-help";
 
 export default function App() {
 	return (
@@ -20,7 +26,26 @@ export default function App() {
 				<Route path="/donate-blood" element={<DonateBloodPage />} />
 				<Route path="/need-blood" element={<NeedBloodPage />} />
 				<Route path="/contact" element={<ContactPage />} />
-				<Route path="/admin/*" element={<Admin />} />
+				<Route path="/admin" element={<Admin />}>
+					<Route index element={<Dashboard />} />
+					<Route
+						path="donate-blood"
+						exact
+						element={<AdminDonateBlood />}
+					/>
+					<Route
+						path="need-blood"
+						exact
+						element={<AdminNeedBlood />}
+					/>
+					<Route
+						path="host-blood-drive"
+						exact
+						element={<AdminHostBloodDrive />}
+					/>
+					<Route path="need-help" exact element={<AdminNeedHelp />} />
+					{/* <Route path="/redirect" element={<Navigate to="/" />} /> */}
+				</Route>
 			</Routes>
 			{/* </BrowserRouter> */}
 			{/* <BeforeFooterCTA />
--- src/components/layouts/admin.jsx
+++ src/components/layouts/admin.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, Outlet } from "react-router-dom";
 
 import Sidebar from "../sidebar/sidebar";
 // import HeaderStats from "../sections/header-stats/header_stats";
 // import AdminNavbar from "../sections/admin-navbar/admin-navbar";
 
-import Dashboard from "../views/admin/dashboard";
-import AdminDonateBlood from "../views/admin/admin-donate-blood";
-import AdminNeedBlood from "../views/admin/admin-need-blood";
-import AdminHostBloodDrive from "../views/admin/admin-host-blood-drive";
-import AdminNeedHelp from "../views/admin/admin-need-help";
+// import Dashboard from "../views/admin/dashboard";
+// import AdminDonateBlood from "../views/admin/admin-donate-blood";
+// import AdminNeedBlood from "../views/admin/admin-need-blood";
+// import AdminHostBloodDrive from "../views/admin/admin-host-blood-drive";
+// import AdminNeedHelp from "../views/admin/admin-need-help";
 
 export default function Admin() {
 	return (
@@ -19,8 +19,9 @@ export default function Admin() {
 				{/* <AdminNavbar /> */}
 				{/* Header */}
 				{/* <HeaderStats /> */}
-				<div className="bg-off_white mx-auto w-full h-full">
-					<Routes>
+				<div className="w-full h-full mx-auto bg-off_white">
+					<Outlet />
+					{/* <Routes>
 						<Route path="/" exact element={<Dashboard />} />
 						<Route
 							path="donate-blood"
@@ -43,7 +44,7 @@ export default function Admin() {
 							element={<AdminNeedHelp />}
 						/>
 						<Route path="/redirect" element={<Navigate to="/" />} />
-					</Routes>
+					</Routes> */}
 					{/* <FooterAdmin /> */}
 				</div>
 			</div>
